682b3f4c9e92d7c260ad0af28fc49bf88f9c0eed
sanmi-owoka <owokaayoola@outlook.com>
2024-01-24 23:20:28 +0100

    Initial project setup

---
+++ config/settings/dev.py
@@ -0,0 +1,9 @@
+# pylint: skip-file
+import dj_database_url
+
+from .base import *
+
+DATABASES = {
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL"), conn_max_age=600)
+}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
