00d43b80cdca5fb00c33e227939d76472cdcf105
sanmi-owoka <owokaayoola@outlook.com>
2024-04-08 22:59:10 +0100

    updated Insurance logic

--- utility/services/pverify.py
+++ utility/services/pverify.py
@@ -4,6 +4,8 @@ import json
 import requests
 from django.conf import settings
 
+from ..helpers.functools import parse_dollar_string_to_number, remove_percentage
+
 
 class Pverify:
     def __init__(self):
@@ -88,8 +90,178 @@ class Pverify:
                     "response": f"{response.json()['ErrorDescription']}",
                 }
 
-            return {"status": True, "response": "Gotten Insurance details successfully"}
+            return {"status": True, "response": response.json()}
 
         except Exception as e:
             print("pverify verify insurance error", e)
-            return None
+            return {
+                "status": False,
+                "response": f"{e}",
+            }
+
+    def complete_insurance_verification(
+        self, obj: dict, patient_relationship: str
+    ) -> dict:
+        try:
+            amount_billed = 350
+            data = obj["HBPC_Deductible_OOP_Summary"]
+            # IndividualDeductibleInNet = data["IndividualDeductibleInNet"]
+            # IndividualDeductibleOutNet = data["IndividualDeductibleOutNet"]
+            # IndividualDeductibleRemainingInNet = data[
+            #     "IndividualDeductibleRemainingInNet"
+            # ]
+            IndividualDeductibleRemainingOutNet = parse_dollar_string_to_number(
+                data["IndividualDeductibleRemainingOutNet"]["Value"]
+            )
+            IndividualOOPRemainingOutNet = parse_dollar_string_to_number(
+                data["IndividualOOPRemainingOutNet"]["Value"]
+            )
+            FamilyDeductibleOutNet = parse_dollar_string_to_number(
+                data["FamilyDeductibleOutNet"]["Value"]
+            )
+            FamilyOOPRemainingOutNet = parse_dollar_string_to_number(
+                data["FamilyOOPRemainingOutNet"]["Value"]
+            )
+
+            UrgentCareSummary = obj["UrgentCareSummary"]
+            ServiceCoveredOutNet = UrgentCareSummary["ServiceCoveredOutNet"]
+            CoPayOutNet = parse_dollar_string_to_number(
+                UrgentCareSummary["CoPayOutNet"]["Value"]
+            )
+            CoInsOutNet = remove_percentage(UrgentCareSummary["CoInsOutNet"]["Value"])
+
+            if ServiceCoveredOutNet == "yes":
+                if patient_relationship == "self":
+                    if IndividualDeductibleRemainingOutNet >= amount_billed:
+                        self_pay = amount_billed
+                        if IndividualOOPRemainingOutNet <= self_pay:
+                            self_pay = IndividualOOPRemainingOutNet
+                            insurance_coverage = amount_billed - self_pay
+                            return {
+                                "status": "success",
+                                "amount": self_pay,
+                                "insurance_coverage": insurance_coverage,
+                            }
+                        else:
+                            insurance_coverage = 0
+                            return {
+                                "status": "success",
+                                "amount": self_pay,
+                                "insurance_coverage": insurance_coverage,
+                            }
+                    else:
+                        self_pay = IndividualDeductibleRemainingOutNet + CoPayOutNet
+                        if self_pay < amount_billed:
+                            self_pay = (
+                                self_pay
+                                + (amount_billed - self_pay) * CoInsOutNet / 100
+                            )
+                            insurance_coverage = amount_billed - self_pay
+                            if IndividualOOPRemainingOutNet <= self_pay:
+                                self_pay = IndividualOOPRemainingOutNet
+                                insurance_coverage = amount_billed - self_pay
+                                return {
+                                    "status": "success",
+                                    "amount": self_pay,
+                                    "insurance_coverage": insurance_coverage,
+                                }
+                            else:
+                                return {
+                                    "status": "success",
+                                    "amount": self_pay,
+                                    "insurance_coverage": insurance_coverage,
+                                }
+                        else:
+                            self_pay = amount_billed
+                            insurance_coverage = 0
+                            return {
+                                "status": "success",
+                                "amount": self_pay,
+                                "insurance_coverage": insurance_coverage,
+                            }
+
+                    # if IndividualOOPRemainingOutNet <= self_pay:
+                    #     self_pay = IndividualOOPRemainingOutNet
+                    #     insurance_coverage = amount_billed - self_pay
+                    #     return {
+                    #         "status": "success",
+                    #         "amount": self_pay,
+                    #         "insurance_coverage": insurance_coverage
+                    #     }
+
+                else:
+                    if FamilyDeductibleOutNet >= amount_billed:
+                        self_pay = amount_billed
+                        insurance_coverage = 0
+                        if FamilyOOPRemainingOutNet <= self_pay:
+                            self_pay = FamilyOOPRemainingOutNet
+                            insurance_coverage = amount_billed - self_pay
+                            return {
+                                "status": "success",
+                                "amount": self_pay,
+                                "insurance_coverage": insurance_coverage,
+                            }
+                        else:
+                            return {
+                                "status": "success",
+                                "amount": self_pay,
+                                "insurance_coverage": insurance_coverage,
+                            }
+
+                    else:
+                        self_pay = FamilyDeductibleOutNet + CoPayOutNet
+                        if self_pay < amount_billed:
+                            self_pay = (
+                                FamilyDeductibleOutNet
+                                + (amount_billed - FamilyDeductibleOutNet)
+                                * CoInsOutNet
+                                / 100
+                            )
+                            insurance_coverage = amount_billed - self_pay
+                            if FamilyOOPRemainingOutNet <= self_pay:
+                                self_pay = FamilyOOPRemainingOutNet
+                                insurance_coverage = amount_billed - self_pay
+                                return {
+                                    "status": "success",
+                                    "amount": self_pay,
+                                    "insurance_coverage": insurance_coverage,
+                                }
+                            else:
+                                return {
+                                    "status": "success",
+                                    "amount": self_pay,
+                                    "insurance_coverage": insurance_coverage,
+                                }
+
+                        else:
+                            self_pay = amount_billed
+                            insurance_coverage = 0
+                            return {
+                                "status": "success",
+                                "amount": self_pay,
+                                "insurance_coverage": insurance_coverage,
+                            }
+
+                    # if FamilyOOPRemainingOutNet <= self_pay:
+                    #     self_pay = FamilyOOPRemainingOutNet
+                    #     insurance_coverage = amount_billed - self_pay
+                    #     return {
+                    #         "status": "success",
+                    #         "amount": self_pay,
+                    #         "insurance_coverage": insurance_coverage
+                    #     }
+
+            else:
+                self_pay = amount_billed
+                insurance_coverage = 0
+                return {
+                    "status": "success",
+                    "amount": self_pay,
+                    "insurance_coverage": insurance_coverage,
+                }
+        except Exception as e:
+            print("pverify verify insurance error", e)
+            return {
+                "status": "failure",
+                "response": f"{e}",
+            }
